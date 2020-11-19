e5f60f1c26fa5adab0d67019a5d18da8c3e8da68
nimesh44 <nimesh.neupane43@gmail.com>
2020-11-19 15:01:00 +0545

    save() method to create users-modelForm()

--- 10. User authentication in CRUD application/user/views.py
+++ 10. User authentication in CRUD application/user/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
@@ -19,6 +19,13 @@ class UserCreateForm(UserCreationForm):
 
 # Create your views here.
 def register_users(request):
+    if request.method == 'POST':
+        data = UserCreateForm(request.POST)
+        if data.is_valid():
+            data.save()
+            return redirect('home')
+
+
     form = UserCreateForm()
     context = {
             'form': form,
