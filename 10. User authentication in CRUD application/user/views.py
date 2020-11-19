4349a2af9c77c7783d23fee3a679379affc20e35
nimesh44 <nimesh.neupane43@gmail.com>
2020-11-19 20:12:22 +0545

    inserting data using create method and hashing password for User model

--- 10. User authentication in CRUD application/user/views.py
+++ 10. User authentication in CRUD application/user/views.py
@@ -6,10 +6,9 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 # Removing help text
-
+# To avoid help text use UserCreateForm modelForm instead of UserCreationForm
 class UserCreateForm(UserCreationForm):
     # email = forms.EmailField(required=True)
-
     def __init__(self, *args, **kwargs):
         super(UserCreateForm, self).__init__(*args, **kwargs)
 
@@ -17,17 +16,36 @@ class UserCreateForm(UserCreationForm):
             self.fields[fieldname].help_text = None
 
 
-# Create your views here.
 def register_users(request):
     if request.method == 'POST':
         data = UserCreateForm(request.POST)
         if data.is_valid():
-            data.save()
+            # To remove help text
+            data = data.cleaned_data
+            user = User.objects.create(username = data['username'])
+            # To hash password
+            user.set_password(data['password1'])
+            user.save()
+
             return redirect('home')
-
-
     form = UserCreateForm()
     context = {
             'form': form,
     }
     return render(request,'user/user_register.html',context)
+
+
+
+# # views to avoid removing help text
+# def register_users(request):
+#     if request.method == 'POST':
+#         data = UserCreationForm(request.POST)
+#         if data.is_valid():
+#             # without removing help text
+#             data.save()
+#             return redirect('home')
+#     form = UserCreationForm()
+#     context = {
+#             'form': form,
+#     }
+#     return render(request,'user/user_register.html',context)
