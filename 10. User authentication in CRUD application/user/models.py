e2867bae2137b5a2e1acc3399ef3b32860abbe12
nimesh44 <nimesh.neupane43@gmail.com>
2020-11-20 11:53:42 +0545

    extend django user model using django proxy model

--- 10. User authentication in CRUD application/user/models.py
+++ 10. User authentication in CRUD application/user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(User):
+    class Meta:
+        proxy = True
+
+    # Proxy model is used if we need to apply method on the model fields
+    def get_full_name(self):
+        return self.first_name + self.last_name
