3a02957ac965d75ad6a54a32947c4a85963ca615
nimesh44 <nimesh.neupane43@gmail.com>
2020-11-21 09:49:03 +0545

    django user model extend using OneToOneField,Profile model

--- 10. User authentication in CRUD application/user/models.py
+++ 10. User authentication in CRUD application/user/models.py
@@ -1,6 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete = models.CASCADE)
+    phone = models.CharField(max_length = 20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 # Create your models here.
 class Profile(User):
     class Meta:
@@ -9,3 +30,4 @@ class Profile(User):
     # Proxy model is used if we need to apply method on the model fields
     def get_full_name(self):
         return self.first_name + self.last_name
+"""
